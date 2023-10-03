12ccfe64d9bd19bfcd1dec4b3af63e2dfa80ff8f
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-10-03 18:32:14 +0300

    Add media queries for the navbar searchBar component

--- components/navBar/index.tsx
+++ components/navBar/index.tsx
@@ -2,40 +2,58 @@ import { FC } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import AuthLinks from "../authLinks";
-// import SearchBar from "../searchBar";
+import SearchBar from "../searchBar";
 import styles from "./navbar.module.css";
 
 const NavBar: FC = () => {
-  const status = false;
+  const status = true;
 
   return (
     <nav className={styles.container}>
       <div className={styles.logo}>
         <Link href="/">Notebook</Link>
       </div>
-      <div className={styles.searchBar}>{/* <SearchBar /> */} SearchBar</div>
+      <div className={styles.searchBar}>
+        <SearchBar />
+      </div>
+      {!status && (
+        <span className={styles.searchIcon}>
+          <i
+            className={`fa-solid fa-magnifying-glass fa-fw ${styles["linksIcon"]}`}
+          ></i>
+        </span>
+      )}
       <div className={styles.links}>
         {!status ? (
           <AuthLinks />
         ) : (
           <>
-            <Link href="/new-post">
+            <span className={styles.searchIcon}>
               <i
-                className={`fa-regular fa-pen-to-square fa-fw ${styles["linksIcon"]}`}
+                className={`fa-solid fa-magnifying-glass fa-fw ${styles["linksIcon"]}`}
               ></i>
-              Write
-            </Link>
+            </span>
+            <div className={styles.write}>
+              <Link href="/new-post">
+                <i
+                  className={`fa-regular fa-pen-to-square fa-fw ${styles["linksIcon"]}`}
+                ></i>
+                Write
+              </Link>
+            </div>
             <Link href="/me/notifications">
               <i
                 className={`fa-regular fa-bell fa-fw ${styles["linksIcon"]}`}
               ></i>
             </Link>
-            <Image
-              src="/profile-pic.png"
-              alt="profile"
-              width={32}
-              height={32}
-            />
+            <div className={styles.profile}>
+              <Image
+                src="/profile-pic.png"
+                alt="profile"
+                width={32}
+                height={32}
+              />
+            </div>
           </>
         )}
       </div>
