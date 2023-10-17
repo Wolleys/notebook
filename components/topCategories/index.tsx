3e3ba587150658fcc3f1983000a6780bbf95b7f9
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-10-17 11:26:44 +0300

    Add topCategories mockup data

--- components/topCategories/index.tsx
+++ components/topCategories/index.tsx
@@ -1,19 +1,7 @@
 import { FC } from "react";
 import Link from "next/link";
 import styles from "./topcategories.module.css";
-
-const categories = [
-  { id: 1, name: "Devotionals" },
-  { id: 2, name: "Bible Study" },
-  { id: 3, name: "Prayer" },
-  { id: 4, name: "Leadership" },
-  { id: 5, name: "Family" },
-  { id: 6, name: "Youth" },
-  { id: 7, name: "Health" },
-  { id: 8, name: "Justice" },
-  { id: 9, name: "Inspiration" },
-  { id: 10, name: "Relationships" },
-];
+import topCategories from "../../mockup/topCategories";
 
 const TopCategories: FC = () => {
   return (
@@ -27,7 +15,7 @@ const TopCategories: FC = () => {
 
       <div className={styles.list}>
         <ul>
-          {categories.map((item) => (
+          {topCategories.slice(0, 10).map((item) => (
             <li key={item.id}>
               <Link href="/">{item.name}</Link>
             </li>
