6fc81e4bc728399e3145e2d3753f9ef3b9d75729
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-10-17 11:16:16 +0300

    Add mostPopular mockup data

--- components/mostPopular/index.tsx
+++ components/mostPopular/index.tsx
@@ -1,45 +1,7 @@
 import { FC } from "react";
 import Link from "next/link";
 import styles from "./mostpopular.module.css";
-
-const popular = [
-  {
-    id: 1,
-    category: "Love",
-    title: "Marketing Tools for Small Business Consulting Firms",
-    author: "Jon Houston",
-    views: 150,
-  },
-  {
-    id: 2,
-    category: "Faith",
-    title: "Love Notes From The Chemo Room",
-    author: "John Doe",
-    views: 100,
-  },
-  {
-    id: 3,
-    category: "Salvation",
-    title: "Are you a victim of generational dieting trauma?",
-    author: "Jane Doe",
-    views: 400,
-  },
-  {
-    id: 4,
-    category: "Jesus",
-    title: "On Regrets and Growth In Sobriety With Holly Whitaker",
-    author: "Nicolina Torres",
-    views: 300,
-  },
-  {
-    id: 5,
-    category: "Praying",
-    title:
-      "Walled gardens mean online writing is dead. Long live online writing!",
-    author: "Kiley Larsen",
-    views: 180,
-  },
-];
+import mostPopular from "../../mockup/mostPopular";
 
 const MostPopular: FC = () => {
   return (
@@ -51,7 +13,7 @@ const MostPopular: FC = () => {
         Trending on Notebook
       </h2>
 
-      {popular.map((item) => (
+      {mostPopular.map((item) => (
         <div className={styles.textContainer} key={item.id}>
           <h5 className={styles.postTitle}>
             <Link href="/">{item.title}</Link>
