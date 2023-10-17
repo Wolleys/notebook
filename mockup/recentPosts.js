3e9a31c29aae0f4bd0b8886240920b78c63ec4a6
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-10-17 11:07:44 +0300

    Add recentPosts mockup data

---
+++ mockup/recentPosts.js
@@ -0,0 +1,71 @@
+const recentPosts = [
+  {
+    id: 1,
+    title: "4 Essential Features for Content Marketing Managers",
+    desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. In sunt,
+      tempora adipisci totam eaque voluptatibus itaque, pariatur ipsum sed
+      maxime voluptatem laborum rerum fuga porro assumenda odit earum error
+      nihil.`,
+    author: "Júlia Araújo",
+    updatedDate: "Oct 17, 2023",
+    categories: ["Love", "Faith"],
+  },
+  {
+    id: 2,
+    title: "Marketing Tools for Small Business Consulting Firms",
+    desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. In sunt,
+      tempora adipisci totam eaque voluptatibus itaque, pariatur ipsum sed
+      maxime voluptatem laborum rerum fuga porro assumenda odit earum error
+      nihil.`,
+    author: "Evelyn Taylor",
+    updatedDate: "Oct 16, 2023",
+    categories: ["Devotionals", "Bible Study"],
+  },
+  {
+    id: 3,
+    title: "Love Notes From The Chemo Room",
+    desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. In sunt,
+      tempora adipisci totam eaque voluptatibus itaque, pariatur ipsum sed
+      maxime voluptatem laborum rerum fuga porro assumenda odit earum error
+      nihil.`,
+    author: "John Doe",
+    updatedDate: "Oct 15, 2023",
+    categories: ["Prayer", "Love"],
+  },
+  {
+    id: 4,
+    title: "Are you a victim of generational dieting trauma?",
+    desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. In sunt,
+      tempora adipisci totam eaque voluptatibus itaque, pariatur ipsum sed
+      maxime voluptatem laborum rerum fuga porro assumenda odit earum error
+      nihil.`,
+    author: "Jane Doe",
+    updatedDate: "Oct 14, 2023",
+    categories: ["Family"],
+  },
+  {
+    id: 5,
+    title: "On Regrets and Growth In Sobriety With Holly Whitaker",
+    desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. In sunt,
+      tempora adipisci totam eaque voluptatibus itaque, pariatur ipsum sed
+      maxime voluptatem laborum rerum fuga porro assumenda odit earum error
+      nihil.`,
+    author: "Nicolina Torres",
+    updatedDate: "Oct 13, 2023",
+    categories: ["Relationships", "Inspiration"],
+  },
+  {
+    id: 6,
+    title:
+      "Walled gardens mean online writing is dead. Long live online writing!",
+    desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. In sunt,
+      tempora adipisci totam eaque voluptatibus itaque, pariatur ipsum sed
+      maxime voluptatem laborum rerum fuga porro assumenda odit earum error
+      nihil.`,
+    author: "Kiley Larsen",
+    updatedDate: "Oct 12, 2023",
+    categories: ["Youth", "Health", "Inspiration"],
+  },
+];
+
+export default recentPosts;
