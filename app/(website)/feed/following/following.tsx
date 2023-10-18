2fbbc67d4e92bc1b766253f7be9e02b399dfafa0
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-10-18 13:23:12 +0300

    Update the feed and following feed components with placehoder posts

--- app/(website)/feed/following/following.tsx
+++ app/(website)/feed/following/following.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import Post from "@/components/post";
 import SideBarMenu from "@/components/sideBarMenu";
 import FeedsTab from "../../../../components/feedsTab";
 import ParentContainer from "@/containers/ParentContainer";
@@ -7,14 +8,7 @@ const Following: FC = () => {
   return (
     <ParentContainer>
       <FeedsTab activeTab="/feed/following">
-        <h4>Following content</h4>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
+        <Post />
       </FeedsTab>
       <SideBarMenu />
     </ParentContainer>
