95b1e471ff52986a372bf0ec8ba7683be7321903
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-10-18 11:20:34 +0300

    Update feedsTab navigation items

--- components/feedsTab/index.tsx
+++ components/feedsTab/index.tsx
@@ -5,6 +5,12 @@ import Link from "next/link";
 import styles from "./feedstab.module.css";
 import { usePathname } from "next/navigation";
 
+const navItems = [
+  { path: "/feed", label: "For you" },
+  { path: "/feed=following", label: "Following" },
+  { path: "/explore", label: "Explore" },
+];
+
 const FeedsTab: FC = () => {
   const pathname = usePathname();
 
@@ -12,133 +18,20 @@ const FeedsTab: FC = () => {
     <div className={styles.container}>
       <div className={styles.tabs}>
         <ul>
-          <li>
-            <Link
-              className={`${pathname === "/feed" ? styles.active : ""}`}
-              href="/"
-            >
-              For you
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={`${
-                pathname === "/?feed=following" ? styles.active : ""
-              }`}
-              href="/"
-            >
-              Following
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={`${pathname === "/explore" ? styles.active : ""}`}
-              href="/"
-            >
-              Explore
-            </Link>
-          </li>
+          {navItems.map((item, index) => (
+            <li key={index}>
+              <Link
+                className={`${pathname === item.path ? styles.active : ""}`}
+                href={item.path}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
       <div className={styles.content}>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
-          molestias consectetur fuga, ut accusamus tenetur totam laborum minima
-          ad, numquam aperiam voluptatem molestiae veritatis ipsum fugit porro
-          repellendus odio?
-        </p>
-        <br />
+        <h4>For You content</h4>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae
           molestias consectetur fuga, ut accusamus tenetur totam laborum minima
