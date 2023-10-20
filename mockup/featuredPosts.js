0c28af0e737f929430a0dbe4a26b1d1994489de5
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-10-20 13:49:45 +0300

    Change dynamic directory name from @username to profile

--- mockup/featuredPosts.js
+++ mockup/featuredPosts.js
@@ -5,6 +5,7 @@ const featuredPosts = [
     desc: `Hostinger is joining the 17th edition of the Awwwards Digital Thinkers Conference as the main sponsor to foster connections in the digital creative community. This prestigious event, known for celebrating innovation in web design, will take place in Amsterdam from 11 to 13 October.`,
     author: "Kotryna D",
     updatedDate: "Sep 29, 2023",
+    username: "kotryna",
   },
 ];
 
