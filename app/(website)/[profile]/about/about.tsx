940a00edab233c333d944df814eaec9c37f0a0e7
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-10-23 10:11:38 +0300

    Update user's about page with navTab component and formatTabs util function

--- app/(website)/[profile]/about/about.tsx
+++ app/(website)/[profile]/about/about.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import { FC } from "react";
-import styles from "./about.module.css";
+import NavTabs from "@/components/navTabs";
+import { useParams } from "next/navigation";
+import { profileTabs } from "@/utils/navItems";
 import SideBarMenu from "@/components/sideBarMenu";
+import { formatProfileTabs } from "@/utils/formatTabs";
 import ParentContainer from "@/containers/parentContainer";
 
 const About: FC = () => {
+  var { profile } = useParams();
+  profile = decodeURIComponent(profile.toString());
+  const tabs = formatProfileTabs(profile, profileTabs);
+
   return (
     <ParentContainer>
-      <div className={styles.container}>
-        <h3>About Page</h3>
-      </div>
+      <NavTabs activeTab={`/${profile}/about`} navItems={tabs}>
+        <h3>{profile} About Page</h3>
+      </NavTabs>
       <SideBarMenu />
     </ParentContainer>
   );
