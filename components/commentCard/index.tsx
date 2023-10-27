21d31366e9885cd7e0833801bf51b7ce5453176d
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-10-27 16:16:33 +0300

    Create interface for the commentCard

--- components/commentCard/index.tsx
+++ components/commentCard/index.tsx
@@ -4,7 +4,21 @@ import Image from "next/image";
 import styles from "./commentcard.module.css";
 import userPic from "../../public/profile-pic.png";
 
-const CommentCard: FC = () => {
+interface Comment {
+  id: number;
+  username: string;
+  date: string;
+  content: string;
+  likes: number;
+  comments: number;
+}
+
+interface CommentCardProps {
+  comment: Comment;
+}
+
+const CommentCard: FC<CommentCardProps> = ({ comment }) => {
+  const { username, date, content, likes, comments } = comment;
   return (
     <div className={styles.comment}>
       <div className={styles.userDetails}>
@@ -13,27 +27,23 @@ const CommentCard: FC = () => {
         </div>
         <div className={styles.userInfo}>
           <span className={styles.username}>
-            <Link href={`/@jane`}>Jane Doe</Link>
+            <Link href={`/@${username}`}>{username}</Link>
           </span>
-          <span className={styles.date}>Oct 27, 2023</span>
+          <span className={styles.date}>{date}</span>
         </div>
       </div>
-      <p className={styles.desc}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, ipsa
-        cumque enim aut voluptates, facilis eum, officiis adipisci quo magnam
-        magni dolor tempore porro officia ducimus beatae. Veritatis, nulla amet?
-      </p>
+      <p className={styles.desc}>{content}</p>
       <div className={styles.dataContainer}>
         <div className={styles.commentData}>
           <span>
             <i className={`fa-regular fa-heart fa-fw ${styles["icons"]}`}></i>
-            <span className={styles.commentNumbers}>30</span>
+            <span className={styles.commentNumbers}>{likes}</span>
           </span>
           <span>
             <i
               className={`fa-regular fa-comment-dots fa-fw ${styles["icons"]}`}
             ></i>
-            <span className={styles.commentNumbers}>5</span>
+            <span className={styles.commentNumbers}>{comments}</span>
           </span>
         </div>
         <>
