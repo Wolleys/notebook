32e0bd9a82c4e977352f62f057115165eb34a612
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-02 16:57:17 +0300

    Add searchIcon to icons list and change interface property name

--- components/fontAwesomeIcon/index.tsx
+++ components/fontAwesomeIcon/index.tsx
@@ -3,12 +3,12 @@ import styles from "./incons.module.css";
 import { IconProps } from "@/interfaces";
 
 interface FAIProps {
-  iconProps: IconProps;
+  icon: IconProps;
   customClass?: string; // Allow custom class name from styles
 }
 
-const FontAwesomeIcon: FC<FAIProps> = ({ iconProps, customClass }) => {
-  const { className, iconName, iconType } = iconProps;
+const FontAwesomeIcon: FC<FAIProps> = ({ icon, customClass }) => {
+  const { className, iconName, iconType } = icon;
   // Use custom class name if provided, otherwise, use the default class from styles
   const dynamicClassName = customClass || styles.icons;
   return (
