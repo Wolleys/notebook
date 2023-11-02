37d1e16e94823af4ed694312305ff7f0eeabaa78
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-02 15:35:12 +0300

    Add icon interface for fontAwesomeIcon component
    > Consume it in the slug page and commentCard component

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,5 +1,7 @@
 import { Post as PostProps } from "./post";
+import { Icon as IconProps } from "./icon";
 import { Comment as CommentInterface } from "./comment";
 
 export type { PostProps };
+export type { IconProps };
 export type { CommentInterface };
