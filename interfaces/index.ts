23a810444b2fdb8d9b696131985cfa2e907905ff
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-03 10:24:10 +0300

    Update comment interface and type it with CommentProps

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,9 +1,9 @@
 import { PostProps } from "./post";
+import { CommentProps } from "./comment";
 import { Icon as IconProps } from "./icon";
 import { FeaturedProps } from "./featuredPost";
-import { Comment as CommentInterface } from "./comment";
 
 export type { PostProps };
 export type { IconProps };
+export type { CommentProps };
 export type { FeaturedProps };
-export type { CommentInterface };
