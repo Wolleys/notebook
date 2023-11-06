db23835a462abc20a03716474eb6684a02adec87
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-06 17:45:11 +0300

    Add about css module file

--- app/(website)/about/about.tsx
+++ app/(website)/about/about.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
+import styles from "./about.module.css";
 
 const About: FC = () => {
   return (
-    <main>
+    <div className={styles.container}>
       <h3>About Page</h3>
-    </main>
+    </div>
   );
 };
 
