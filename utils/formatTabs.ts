c8547e794add26468f5e2282cec93ccbfeb659ec
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-06 09:27:50 +0300

    Use the TabProps interface for tabs instead of creating a seperate one

--- utils/formatTabs.ts
+++ utils/formatTabs.ts
@@ -1,10 +1,7 @@
-interface TabItem {
-  path: string;
-  label: string;
-}
+import { TabProps } from "@/interfaces";
 
 // If profile username is available, replace placeholders with the actual username
-export const formatProfileTabs = (profile: string, tabs: TabItem[]) => {
+export const formatProfileTabs = (profile: string, tabs: TabProps[]) => {
   return tabs.map((tab) => ({
     ...tab,
     path: tab.path.replace("/[profile]", `/${profile}`),
