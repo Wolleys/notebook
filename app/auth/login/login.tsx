58a91821d9e3bc7f81d27e4f2f019cdf9a1e86dd
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-07 17:55:05 +0300

    Add sign in input and button

--- app/auth/login/login.tsx
+++ app/auth/login/login.tsx
@@ -3,9 +3,25 @@ import styles from "./login.module.css";
 
 const Login: FC = () => {
   return (
-    <div className={styles.container}>
-      <h3>Sign In to Notebook</h3>
-    </div>
+    <section className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.textContainer}>
+          <h1>Sign In to Notebook</h1>
+          <p>Enter your email below to sign in</p>
+        </div>
+        <div className={styles.formContainer}>
+          <input
+            type="email"
+            name="email"
+            autoCorrect="off"
+            autoComplete="off"
+            className={styles.input}
+            placeholder="name@example.com"
+          />
+          <button className={styles.button}> Sign In with Email</button>
+        </div>
+      </div>
+    </section>
   );
 };
 
