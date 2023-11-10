8dcd4b224c8ad43ca6f20206f6bfe6bdf0524996
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-10 09:27:14 +0300

    Add first and last name inputs to the inputs list

--- inputs/index.ts
+++ inputs/index.ts
@@ -3,5 +3,17 @@ import { InputProps } from "@/interfaces";
 export const email: InputProps = {
   type: "email",
   name: "email",
-  placeholder: "name@example.com",
+  placeholder: "Enter your email",
+};
+
+export const firstName: InputProps = {
+  type: "text",
+  name: "firstName",
+  placeholder: "First name",
+};
+
+export const lastName: InputProps = {
+  type: "text",
+  name: "lastName",
+  placeholder: "Last name",
 };
