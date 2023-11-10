538a110a7c615fc5a9ca447f4ba0e0b1d145dce8
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-10 11:25:35 +0300

    Add signUpOptions component and consumme in both auth pages

--- app/auth/login/login.tsx
+++ app/auth/login/login.tsx
@@ -4,8 +4,7 @@ import { email } from "@/inputs";
 import Input from "@/components/input";
 import styles from "./login.module.css";
 import Button from "@/components/button";
-import FontAwesomeIcon from "@/components/fontAwesomeIcon";
-import { googleIcon, facebookIcon, xIcon, appleIcon } from "@/icons";
+import SignUpOptions from "@/components/signupOptions";
 
 const Login: FC = () => {
   return (
@@ -24,20 +23,7 @@ const Login: FC = () => {
           <span className={styles.text}>Or continue with</span>
           <div className={styles.line}></div>
         </div>
-        <div className={styles.socialContainer}>
-          <Button variant="social">
-            <FontAwesomeIcon icon={googleIcon} /> Google
-          </Button>
-          <Button variant="social">
-            <FontAwesomeIcon icon={facebookIcon} /> Facebook
-          </Button>
-          <Button variant="social">
-            <FontAwesomeIcon icon={xIcon} /> Twitter
-          </Button>
-          <Button variant="social">
-            <FontAwesomeIcon icon={appleIcon} /> Apple
-          </Button>
-        </div>
+        <SignUpOptions />
         <div className={styles.divider}>
           <span className={styles.text}>
             Not been here before? Just
