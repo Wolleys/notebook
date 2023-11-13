e73dad9fd1c2d8089922721de15f26ba58f94970
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-13 14:47:25 +0300

    Add an onChange event listener to the input interface

--- app/auth/login/login.tsx
+++ app/auth/login/login.tsx
@@ -1,28 +1,38 @@
+"use client";
+
 import { FC } from "react";
 import { email } from "@/inputs";
 import Input from "@/components/input";
 import Button from "@/components/button";
+import useInputChange from "@/hooks/useInputChange";
 import AuthContainer from "@/containers/authContainer";
 
-const fields = [
-  <Input key="email" input={email} />,
-  <Button key="button" variant="auth">
-    Sign In with Email
-  </Button>,
-];
-
-const loginProps = {
-  title: "Sign In to Notebook",
-  desc: "Enter your email below to sign in",
-  fields,
-  bottomText: "Not been here before? Just",
-  bottomLink: {
-    href: "/auth/register",
-    text: "Sign Up",
-  },
-};
-
 const Login: FC = () => {
+  const initialFormState = { email: "" };
+  const { values, handleInputChange } = useInputChange({
+    initialValues: initialFormState,
+  });
+
+  console.log(values.email);
+
+  const fields = [
+    <Input key="email" input={email} onChange={handleInputChange} />,
+    <Button key="button" variant="auth">
+      Sign In with Email
+    </Button>,
+  ];
+
+  const loginProps = {
+    title: "Sign In to Notebook",
+    desc: "Enter your email below to sign in",
+    fields,
+    bottomText: "Not been here before? Just",
+    bottomLink: {
+      href: "/auth/register",
+      text: "Sign Up",
+    },
+  };
+
   return <AuthContainer props={loginProps} />;
 };
 
