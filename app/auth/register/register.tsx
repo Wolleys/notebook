73d459ddcfcf2d34b8725fe56c98d72e3b99d6e9
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-13 15:56:54 +0300

    Update register page to consume the useInputChange hook

--- app/auth/register/register.tsx
+++ app/auth/register/register.tsx
@@ -1,34 +1,48 @@
-import { FC } from "react";
+"use client";
+
+import { FC, FormEvent } from "react";
 import Input from "@/components/input";
 import Button from "@/components/button";
+import useInputChange from "@/hooks/useInputChange";
 import { email, firstName, lastName } from "@/inputs";
 import AuthContainer from "@/containers/authContainer";
 
-const nameFields = [
-  <Input key="firstName" input={firstName} />,
-  <Input key="lastName" input={lastName} />,
-];
-
-const fields = [
-  <Input key="email" input={email} />,
-  <Button key="button" variant="auth">
-    Sign Up with Email
-  </Button>,
-];
-
-const registerProps = {
-  title: "Sign Up to Notebook",
-  desc: "Enter your email below to sign up",
-  nameFields,
-  fields,
-  bottomText: "Already been here before? Just",
-  bottomLink: {
-    href: "/auth/login",
-    text: "Sign In",
-  },
-};
-
 const Register: FC = () => {
+  const initialFormState = { email: "", firstName: "", lastName: "" };
+  const { values, handleInputChange } = useInputChange({
+    initialValues: initialFormState,
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form values:", values);
+  };
+
+  const nameFields = [
+    <Input key="firstName" input={firstName} onChange={handleInputChange} />,
+    <Input key="lastName" input={lastName} onChange={handleInputChange} />,
+  ];
+
+  const fields = [
+    <Input key="email" input={email} onChange={handleInputChange} />,
+    <Button key="button" variant="auth">
+      Sign Up with Email
+    </Button>,
+  ];
+
+  const registerProps = {
+    title: "Sign Up to Notebook",
+    desc: "Enter your email below to sign up",
+    nameFields,
+    fields,
+    bottomText: "Already been here before? Just",
+    bottomLink: {
+      href: "/auth/login",
+      text: "Sign In",
+    },
+    onSubmit: handleSubmit,
+  };
+
   return <AuthContainer props={registerProps} />;
 };
 
