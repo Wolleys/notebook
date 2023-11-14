8a8a5216744468c3e6da6d725f77515a4c7806c4
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-14 17:41:38 +0300

    Add other editor modules

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -11,14 +11,16 @@ interface EditorProps {
 const Editor: FC<EditorProps> = ({ value, onChange }) => {
   const modules = {
     toolbar: [
-      [{ header: [1, 2, false] }],
       ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ script: "sub" }, { script: "super" }],
       [
-        { list: "ordered" },
         { list: "bullet" },
+        { list: "ordered" },
         { indent: "-1" },
         { indent: "+1" },
       ],
+      [{ align: [] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
       ["link", "image"],
       ["clean"],
     ],
