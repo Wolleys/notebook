dee47d3cc13999a8b5e03a87875022d874c57a7a
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-11-20 15:05:22 +0300

    Add postTitle input, select and file inputs to new post page

--- app/(website)/new-post/new-post.tsx
+++ app/(website)/new-post/new-post.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { FC, useState } from "react";
+import { postTitle } from "@/inputs";
+import Input from "@/components/input";
 import Editor from "@/components/editor";
 import styles from "./newpost.module.css";
 
@@ -9,6 +11,20 @@ const NewPost: FC = () => {
 
   return (
     <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Input input={postTitle} />
+        <div className={styles.split}>
+          <select className={styles.select}>
+            <option>Select Cartegory</option>
+            <option value="fashion">fashion</option>
+            <option value="food">food</option>
+            <option value="culture">culture</option>
+            <option value="travel">travel</option>
+            <option value="coding">coding</option>
+          </select>
+          <input type="file" />
+        </div>
+      </div>
       <Editor value={value} onChange={setValue} />
     </div>
   );
