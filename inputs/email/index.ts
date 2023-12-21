bb28871eabba565c623f61e04cf5774f7047610d
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-12-21 11:02:12 +0300

    Add email input to inputs list

---
+++ inputs/email/index.ts
@@ -0,0 +1,19 @@
+import { IInput } from "@/components/input/IInput";
+
+/**
+ * Email input.
+ */
+export const email: IInput = {
+  /**
+   * Input of type email.
+   */
+  type: "email",
+  /**
+   * Input name.
+   */
+  name: "email",
+  /**
+   * Input placehiolder.
+   */
+  placeholder: "Enter your email",
+};
