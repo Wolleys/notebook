e091d8f0b55e8901e85c803504cfeb4cfc8741b1
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-12-21 11:12:45 +0300

    Add last name input to inputs list

---
+++ inputs/lastName/index.ts
@@ -0,0 +1,19 @@
+import { IInput } from "@/components/input/IInput";
+
+/**
+ * Last name input.
+ */
+export const lastName: IInput = {
+  /**
+   * Input of type text.
+   */
+  type: "text",
+  /**
+   * Input name.
+   */
+  name: "lastName",
+  /**
+   * Input placehiolder.
+   */
+  placeholder: "Last name",
+};
---
+++ inputs/postTitle/index.ts
@@ -0,0 +1,19 @@
+import { IInput } from "@/components/input/IInput";
+
+/**
+ * Post title input.
+ */
+export const postTitle: IInput = {
+  /**
+   * Input of type text.
+   */
+  type: "text",
+  /**
+   * Input name.
+   */
+  name: "postTitle",
+  /**
+   * Input placehiolder.
+   */
+  placeholder: "Title",
+};
