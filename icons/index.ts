7421d90d20d0c93199f3888f156ac48487075110
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-12-21 13:08:39 +0300

    Update commentCard component icons imports

--- icons/index.ts
+++ icons/index.ts
@@ -1,14 +1,14 @@
 import { IconProps } from "@/interfaces";
 
-export const heartIcon: IconProps = {
-  iconName: "heart",
-  iconType: "fa-regular",
-};
+// export const heartIcon: IconProps = {
+//   iconName: "heart",
+//   iconType: "fa-regular",
+// };
 
-export const commentIcon: IconProps = {
-  iconName: "comment-dots",
-  iconType: "fa-regular",
-};
+// export const commentIcon: IconProps = {
+//   iconName: "comment-dots",
+//   iconType: "fa-regular",
+// };
 
 // export const bookmarkIcon: IconProps = {
 //   iconName: "bookmark",
