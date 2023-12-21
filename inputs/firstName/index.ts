2fa44db9f634b8d5b55e3057d179a4dc4921ce5e
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-12-21 11:08:51 +0300

    Add first name input to inputs list

---
+++ inputs/firstName/index.ts
@@ -0,0 +1,19 @@
+import { IInput } from "@/components/input/IInput";
+
+/**
+ * First name input.
+ */
+export const firstName: IInput = {
+  /**
+   * Input of type text.
+   */
+  type: "text",
+  /**
+   * Input name.
+   */
+  name: "firstName",
+  /**
+   * Input placehiolder.
+   */
+  placeholder: "First name",
+};
