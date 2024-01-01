e303a9e845477293c82c8c6cd37d3aec1e6199c9
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-01-01 21:59:14 +0300

    Add search bar input to inputs list

---
+++ inputs/searchBar/index.ts
@@ -0,0 +1,19 @@
+import { IInput } from "@/components/input/IInput";
+
+/**
+ * Search bar input.
+ */
+export const searchBar: IInput = {
+  /**
+   * Input of type text.
+   */
+  type: "text",
+  /**
+   * Input name.
+   */
+  name: "searchBar",
+  /**
+   * Input placehiolder.
+   */
+  placeholder: "Search Notebook",
+};
