2c2fd7282dbb19366a987a17125850dbc09d811d
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-01-04 09:00:17 +0300

    Conditinally render publish button

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,15 +1,24 @@
+"use client";
+
 import { FC } from "react";
 import Link from "next/link";
 import SearchBar from "../searchBar";
 import SearchIcon from "../searchIcon";
 import styles from "./header.module.css";
+import Button from "@/components/button";
 import { penIcon } from "@/icons/penIcon";
 import HeaderAvatar from "../headerAvatar";
 import { bellIcon } from "@/icons/bellIcon";
+import { usePathname } from "next/navigation";
 import FontAwesomeIcon from "@/components/fontAwesomeIcon";
 
 const Header: FC = () => {
   const status = true;
+  const pathname = usePathname();
+
+  const handlePublish = () => {
+    console.log("Note published");
+  };
 
   return (
     <div className={styles.container}>
@@ -51,15 +60,23 @@ const Header: FC = () => {
                   </span>
                 </li>
                 <li>
-                  <span className={styles.write}>
-                    <Link href="/new-post">
-                      <FontAwesomeIcon
-                        icon={penIcon}
-                        customClass={styles.linksIcon}
-                      />
-                      &nbsp;Write
-                    </Link>
-                  </span>
+                  {pathname === "/new-post" ? (
+                    <span className={styles.write}>
+                      <Button variant="publish" onClick={handlePublish}>
+                        Publish
+                      </Button>
+                    </span>
+                  ) : (
+                    <span className={styles.write}>
+                      <Link href="/new-post">
+                        <FontAwesomeIcon
+                          icon={penIcon}
+                          customClass={styles.linksIcon}
+                        />
+                        &nbsp;Write
+                      </Link>
+                    </span>
+                  )}
                 </li>
                 <li>
                   <Link href="/me/notifications">
