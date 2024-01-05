dd3d5df047dbdf792f7dd9ee685c6f05c7bfb96f
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-01-05 09:18:14 +0300

    Add icon interface and font awesome icon props files

---
+++ components/fontAwesomeIcon/IIcon.ts
@@ -0,0 +1,17 @@
+/**
+ * Icon interface.
+ */
+export interface IIcon {
+  /**
+   * Icon name.
+   */
+  iconName: string;
+  /**
+   * Icon type.
+   */
+  iconType: string;
+  /**
+   * Icon class name from css file
+   */
+  className?: string;
+}
---
+++ components/fontAwesomeIcon/fontAwesomeIconProps.ts
@@ -0,0 +1,12 @@
+import { IIcon } from "./IIcon";
+
+export interface FontAwesomeIconProps {
+  /**
+   * Icon interface.
+   */
+  icon: IIcon;
+  /**
+   * Allow custom class name from styles.
+   */
+  customClass?: string;
+}
