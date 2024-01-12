eb21aba7071289e48785d506f9f38357a41de0ca
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-01-12 16:56:53 +0300

    Add JSDoc comments to editor props

--- components/editor/EditorProps.ts
+++ components/editor/EditorProps.ts
@@ -1,4 +1,13 @@
+/**
+ * Editor props.
+ */
 export interface EditorProps {
+  /**
+   * Value from the input.
+   */
   value: string;
+  /**
+   * onChange event listener.
+   */
   onChange: (value: string) => void;
 }
