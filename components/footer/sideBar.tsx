0e48a11e99274659c26bbf8c6f4af9a227db2acf
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-01-18 07:04:31 +0300

    Move sideBarFooterlinks to a seperate file

--- components/footer/sideBar.tsx
+++ components/footer/sideBar.tsx
@@ -1,17 +1,9 @@
 import { FC } from "react";
 import Link from "next/link";
 import styles from "./footer.module.css";
+import { sideBarFooterlinks } from "./sideBarFooterlinks";
 
 const SideBarFooter: FC = () => {
-  const sideBarFooterlinks = [
-    { path: "/about", label: "About" },
-    { path: "/help", label: "Help" },
-    { path: "/contact", label: "Contact" },
-    { path: "/terms", label: "Terms" },
-    { path: "/privacy", label: "Privacy" },
-    { path: "/status", label: "Status" },
-  ];
-
   return (
     <div className={styles.sideBarFooter}>
       <div className={styles.sideBarFooterLinks}>
@@ -21,21 +13,6 @@ const SideBarFooter: FC = () => {
               <Link href={link.path}>{link.label}</Link>
             </li>
           ))}
-          {/* <li>
-            <Link href="/help">Help</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link href="/terms">Terms</Link>
-          </li>
-          <li>
-            <Link href="/privacy">Privacy</Link>
-          </li>
-          <li>
-            <Link href="/status">Status</Link>
-          </li> */}
         </ul>
       </div>
       <p>© Notebook, Inc. All rights reserved.</p>
---
+++ components/footer/sideBarFooterlinks.ts
@@ -0,0 +1,11 @@
+/**
+ * Sidebar footer links.
+ */
+export const sideBarFooterlinks = [
+  { path: "/about", label: "About" },
+  { path: "/help", label: "Help" },
+  { path: "/contact", label: "Contact" },
+  { path: "/terms", label: "Terms" },
+  { path: "/privacy", label: "Privacy" },
+  { path: "/status", label: "Status" },
+];
