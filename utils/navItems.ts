f51a36ac16b0d7881377f254814e1f96a448f7ac
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-01-25 10:16:00 +0300

    Update navItems utils import path

--- utils/navItems.ts
+++ utils/navItems.ts
@@ -1,12 +1,12 @@
-import { TabProps } from "@/interfaces";
+import { INavTabs } from "@/components/navTabs/INavTabs";
 
-export const feedsTabs: TabProps[] = [
+export const feedsTabs: INavTabs[] = [
   { path: "/feed", label: "For you" },
   { path: "/feed/following", label: "Following" },
   { path: "/explore", label: "Explore" },
 ];
 
-export const profileNavTabs: TabProps[] = [
+export const profileNavTabs: INavTabs[] = [
   { path: "/[profile]", label: "Home" },
   { path: "/[profile]/lists", label: "Lists" },
   { path: "/[profile]/about", label: "About" },
