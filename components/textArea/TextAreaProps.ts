3689d47d416d001bbe9d6798cbfdc9ef884c1585
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-01-30 18:01:17 +0300

    Update JSDoc comments for textarea props

--- components/textArea/TextAreaProps.ts
+++ components/textArea/TextAreaProps.ts
@@ -1,5 +1,17 @@
+/**
+ * Textarea props.
+ */
 export interface TextAreaProps {
+  /**
+   * Checks if we are replying.
+   */
   isReply: boolean;
+  /**
+   * Name of user we are replying to.
+   */
   replyingToUser?: string;
+  /**
+   * Fuction that sends the post.
+   */
   onPost: (text: string) => void;
 }
