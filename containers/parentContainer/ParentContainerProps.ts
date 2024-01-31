9a53f28c0056c4642888eddfb38167bde727f9d4
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-01-31 09:37:59 +0300

    Update parent container props JSDoc comments

--- containers/parentContainer/ParentContainerProps.ts
+++ containers/parentContainer/ParentContainerProps.ts
@@ -1,6 +1,15 @@
 import { ReactNode } from "react";
 
+/**
+ * Parent container props.
+ */
 export interface ParentContainerProps {
+  /**
+   * Chidren elements to be rendered.
+   */
   children: ReactNode;
+  /**
+   * Decide whether to show the featured categories.
+   */
   showFeatured?: boolean;
 }
