bcc1030207dcb21f0a790381d1c90abcd5dbddae
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-02-01 22:26:17 +0300

    Update import in the bellIcon list

--- icons/bellIcon/index.ts
+++ icons/bellIcon/index.ts
@@ -1,6 +1,12 @@
-import { IconProps } from "@/interfaces";
+import { IIcon } from "@/components/fontAwesomeIcon/IIcon";
 
-export const bellIcon: IconProps = {
+export const bellIcon: IIcon = {
+  /**
+   * Icon name.
+   */
   iconName: "bell",
+  /**
+   * Icon type.
+   */
   iconType: "fa-regular",
 };
