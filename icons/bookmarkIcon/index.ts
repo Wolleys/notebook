5ae7557285d8197c6e61993d806ebeec119c1281
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-02-02 19:29:21 +0300

    Update import in the bookmarkIcon icons list

--- icons/bookmarkIcon/index.ts
+++ icons/bookmarkIcon/index.ts
@@ -1,6 +1,12 @@
-import { IconProps } from "@/interfaces";
+import { IIcon } from "@/components/fontAwesomeIcon/IIcon";
 
-export const bookmarkIcon: IconProps = {
+export const bookmarkIcon: IIcon = {
+  /**
+   * Icon name.
+   */
   iconName: "bookmark",
+  /**
+   * Icon type.
+   */
   iconType: "fa-regular",
 };
