8919afe4fffb3e7618506c5abfe055e8e34d0dab
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-02-05 10:11:46 +0300

    Update import in the eyeIcon icons list

--- icons/eyeIcon/index.ts
+++ icons/eyeIcon/index.ts
@@ -1,6 +1,12 @@
-import { IconProps } from "@/interfaces";
+import { IIcon } from "@/components/fontAwesomeIcon/IIcon";
 
-export const eyeIcon: IconProps = {
+export const eyeIcon: IIcon = {
+  /**
+   * Icon name.
+   */
   iconName: "eye",
+  /**
+   * Icon type.
+   */
   iconType: "fa-regular",
 };
