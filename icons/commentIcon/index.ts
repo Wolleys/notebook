9dc1cee060798b5153d8704d335ad5fca798cc86
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-02-05 10:10:27 +0300

    Update import in the comentIcon icons list

--- icons/commentIcon/index.ts
+++ icons/commentIcon/index.ts
@@ -1,6 +1,12 @@
-import { IconProps } from "@/interfaces";
+import { IIcon } from "@/components/fontAwesomeIcon/IIcon";
 
-export const commentIcon: IconProps = {
+export const commentIcon: IIcon = {
+  /**
+   * Icon name.
+   */
   iconName: "comment-dots",
+  /**
+   * Icon type.
+   */
   iconType: "fa-regular",
 };
