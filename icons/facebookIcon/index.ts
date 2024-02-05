af11638f33049ce966cab7a3482462e28bb2cda1
Wolleys Migaya <wolleysmigaya@gmail.com>
2024-02-05 10:16:04 +0300

    Update import in the facebookIcon icons list

--- icons/facebookIcon/index.ts
+++ icons/facebookIcon/index.ts
@@ -1,6 +1,12 @@
-import { IconProps } from "@/interfaces";
+import { IIcon } from "@/components/fontAwesomeIcon/IIcon";
 
-export const facebookIcon: IconProps = {
+export const facebookIcon: IIcon = {
+  /**
+   * Icon name.
+   */
   iconName: "facebook",
+  /**
+   * Icon type.
+   */
   iconType: "fa-brands",
 };
