45e99fa6c05b5390e95ba628e042dd03df9f74e0
Wolleys <wolleysmigaya@gmail.com>
2024-02-06 08:09:15 +0300

    Update import in the heartIcon icons list

--- icons/heartIcon/index.ts
+++ icons/heartIcon/index.ts
@@ -1,6 +1,12 @@
-import { IconProps } from "@/interfaces";
+import { IIcon } from "@/components/fontAwesomeIcon/IIcon";
 
-export const heartIcon: IconProps = {
+export const heartIcon: IIcon = {
+  /**
+   * Icon name.
+   */
   iconName: "heart",
+  /**
+   * Icon type.
+   */
   iconType: "fa-regular",
 };
