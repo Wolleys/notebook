2b324c4648739293b7075c724d9b75b820f8c22e
Wolleys <wolleysmigaya@gmail.com>
2024-02-07 19:19:44 +0300

    Update import in the imageIcon icons list

--- icons/imageIcon/index.ts
+++ icons/imageIcon/index.ts
@@ -1,6 +1,12 @@
-import { IconProps } from "@/interfaces";
+import { IIcon } from "@/components/fontAwesomeIcon/IIcon";
 
-export const imageIcon: IconProps = {
+export const imageIcon: IIcon = {
+  /**
+   * Icon name.
+   */
   iconName: "image",
+  /**
+   * Icon type.
+   */
   iconType: "fa-regular",
 };
