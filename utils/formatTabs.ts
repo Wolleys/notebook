3ee0cc72b643f93650d4d9ab6e69dd23ee57f084
Wolleys <wolleysmigaya@gmail.com>
2024-02-27 18:44:38 +0300

    Del formartTabs util function

--- utils/formatTabs.ts
+++
@@ -1,8 +0,0 @@
-import { ProfileTabsProps } from "@/interfaces";
-
-export const profileTabs = ({ profile, tabs }: ProfileTabsProps) => {
-  return tabs.map((tab) => ({
-    ...tab,
-    path: tab.path.replace("/[profile]", `/${profile}`),
-  }));
-};
