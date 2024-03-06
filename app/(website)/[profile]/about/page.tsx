428429b2361f115192ec50218b86311de65af123
Wolleys <wolleysmigaya@gmail.com>
2024-03-06 09:28:53 +0300

    Add JSDoc comments to the profile about page

--- app/(website)/[profile]/about/page.tsx
+++ app/(website)/[profile]/about/page.tsx
@@ -2,11 +2,18 @@ import { FC } from "react";
 import About from "./about";
 import type { Metadata } from "next";
 
+/**
+ * The metadata for the profile about page.
+ */
 export const metadata: Metadata = {
   title: "About - Username - Notebook",
   description: "This is the notebook app",
 };
 
+/**
+ * Component which shows the profile about page.
+ * @returns The profile about page component.
+ */
 const AboutPage: FC = () => {
   return <About />;
 };
