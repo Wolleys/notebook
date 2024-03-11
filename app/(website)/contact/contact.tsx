b07fef072d4a7e0b2b038eafc6357d3c279d4f60
Wolleys <wolleysmigaya@gmail.com>
2024-03-11 09:35:52 +0300

    Add JSDoc comments to the contact page

--- app/(website)/contact/contact.tsx
+++ app/(website)/contact/contact.tsx
@@ -1,6 +1,10 @@
 import { FC } from "react";
 import styles from "./contact.module.css";
 
+/**
+ * Component which shows the contact page.
+ * @returns The contact page component.
+ */
 const Contact: FC = () => {
   return (
     <div className={styles.container}>
--- app/(website)/contact/page.tsx
+++ app/(website)/contact/page.tsx
@@ -2,11 +2,18 @@ import { FC } from "react";
 import Contact from "./contact";
 import type { Metadata } from "next";
 
+/**
+ * The metadata for the contact page.
+ */
 export const metadata: Metadata = {
   title: "Contact – Notebook",
   description: "This is the notebook app contact us page",
 };
 
+/**
+ * Component which shows the contact page.
+ * @returns The contact page component.
+ */
 const ContactPage: FC = () => {
   return <Contact />;
 };
