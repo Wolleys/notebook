7f977268a2b3f60883d570f39cba12ab1dca4d10
Wolleys <wolleysmigaya@gmail.com>
2024-03-20 09:06:55 +0300

    Add JSDoc comments to the search page

--- app/(website)/search/page.tsx
+++ app/(website)/search/page.tsx
@@ -2,11 +2,18 @@ import { FC } from "react";
 import Search from "./search";
 import type { Metadata } from "next";
 
+/**
+ * The metadata for the search page.
+ */
 export const metadata: Metadata = {
   title: "Search page",
-  description: "This is the notebook app Search page",
+  description: "This is the notebook app search page",
 };
 
+/**
+ * Component which shows the search page.
+ * @returns The search page component.
+ */
 const SearchPage: FC = () => {
   return <Search />;
 };
--- app/(website)/search/search.tsx
+++ app/(website)/search/search.tsx
@@ -4,6 +4,10 @@ import { FC } from "react";
 import styles from "./search.module.css";
 import { useSearchParams } from "next/navigation";
 
+/**
+ * Component which shows the search page.
+ * @returns The search page component.
+ */
 const Search: FC = () => {
   const { get } = useSearchParams();
   const search = get("q");
