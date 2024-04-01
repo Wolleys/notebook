31bf7b1b87e64e11ff01ac38f187a9a84b25ad3e
Wolleys <wolleysmigaya@gmail.com>
2024-04-01 20:45:15 +0300

    Add JSDoc comments to useClickOutside hook

--- hooks/useClickOutside/index.ts
+++ hooks/useClickOutside/index.ts
@@ -2,8 +2,20 @@ import { RefObject, useEffect } from "react";
 
 type RefType = RefObject<HTMLElement>;
 
+/**
+ * Custom hook that invokes a callback when a click occurs outside the referenced element.
+ * @param ref - Reference to the DOM element to watch for clicks outside.
+ * @param callback - Callback function to be invoked when a click occurs outside the referenced element.
+ * @returns {void}
+ */
 const useClickOutside = (ref: RefType, callback: () => void): void => {
   useEffect(() => {
+    /**
+     * Event handler that checks if the click occurs outside the referenced element
+     * and invokes the callback if so.
+     * @param event - The mouse event object.
+     * @returns {void}
+     */
     const handleClickOutside = (event: MouseEvent): void => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
         callback();
