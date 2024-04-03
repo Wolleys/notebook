cbe797b81620bbecb7bcdf8f15e7d9ff8e9396cd
Wolleys <wolleysmigaya@gmail.com>
2024-04-03 21:03:33 +0300

    Add JSDoc comments to useToggle hook

--- hooks/useToggle/index.ts
+++ hooks/useToggle/index.ts
@@ -5,9 +5,17 @@ type UseToggleReturnType = {
   toggle: () => void;
 };
 
+/**
+ * Hook for toggling a boolean value.
+ * @param initialValue - The initial value of the toggle.
+ * @returns - An object containing the current value and a function to toggle it.
+ */
 const useToggle = (initialValue: boolean): UseToggleReturnType => {
   const [value, setValue] = useState<boolean>(initialValue);
 
+  /**
+   * Toggle the boolean value.
+   */
   const toggle = (): void => {
     setValue((prevValue) => !prevValue);
   };
